7c905a322339a1b50139ff40f92d3c17e3450b21
Darin Costello <dran226@gmail.com>
2017-07-07 19:48:25 -0600

    Built bluetooth wrapper.

--- spheropy.py
+++ spheropy.py
@@ -1,4 +1,108 @@
 #!/usr/bin/python
-""" Python module allowing the control of a Sphero 2.0"""
-import bluetooth
+"""
+Python module allowing the control of a Sphero 2.0
+"""
+import re
+import sys
 import threading
+
+import bluetooth
+
+
+class SpheroException(Exception):
+    """ Exception class for the Sphero"""
+    pass
+
+
+class BluetoothWrapper(object):
+    """
+    A class used to wrap the bluetooth connection to the sphero
+    """
+
+    @classmethod
+    def find_free_spheros(cls, tries=5, regex="[Ss]phero"):
+        """
+        Finds a list of all available spheros
+        @param tries: indicates the number of times to scan for bluetooth devices
+        @param regex: A regex used to match bluetooth device names,
+        @return: A dictionary from names to addresses
+        """
+        re_prog = re.compile(regex)
+        result = {}
+        for _ in range(0, tries):
+            devices_in_range = bluetooth.discover_devices(lookup_names=True)
+            for addr, name in devices_in_range:
+                if re.match(re_prog, name):
+                    result[name] = addr
+
+        return result
+
+    def __init__(self, name="Sphero", address=None, port=1):
+        self.name = name
+        self.address = address
+        self.port = port
+        self._socket = None
+
+    def connect(self, address=None):
+        """
+        Connects, attempts to connect to a sphero, address must be set, or given as a keyword argument. If a connection is all ready made it is closed, and a new one is started.
+        """
+        if address is not None:
+            self.address = address
+
+        if not self._socket is not None:
+            self._socket.close()
+
+        try:
+            self._socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+            self._socket.connect((self.address, self.port))
+        except bluetooth.BluetoothError as error:
+            if self._socket is not None:
+                self._socket.close()
+            self._socket = None
+            sys.stderr.write(error.message + '\n')
+            sys.stderr.flush()
+            raise SpheroException(
+                "Unable to connect due to bluetooth error: " + error.message)
+
+    def send(self, msg):
+        """
+        sends a message to the sphero
+        @param msg: a byte string
+        Blocks until message is sent.
+        """
+        if self._socket is None:
+            raise SpheroException("Sphero is not connected")
+
+        message_len = len(msg)
+        while message_len > 0:
+            try:
+                sent_amount = self._socket.send(msg)
+            except bluetooth.BluetoothError as error:
+                raise SpheroException(
+                    "Unable to send all data, due to bluetooth error: " + error.message)
+            message_len -= sent_amount
+            msg = msg[sent_amount:]
+
+    def receive(self, num_bytes):
+        """
+        recieves data from Sphero, and returns it as a byte string
+        @param num_bytes, the number of bytes to request
+        @return a byte string with length less than num_bytes, when sphero is disconnected, and all data is read, the empty string is returned. 
+        Blocks until atleast one byte is available.
+        """
+        if self._socket is None:
+            raise SpheroException("Sphero is not connected")
+        try:
+            return self._socket.recv(num_bytes)
+        except bluetooth.BluetoothError as error:
+            raise SpheroException(
+                "Unable to receive data due to bluetooth error: " + error.message)
+
+    def close(self):
+        """
+        Closes the connection to sphero
+        """
+        if self._socket is not None:
+            self._socket.close()
+            self._socket = None
---
+++ test.py
@@ -0,0 +1,72 @@
+# pylint: disable=c0111
+import unittest
+from spheropy import BluetoothWrapper, SpheroException
+import bluetooth
+
+
+class OverrideDiscover(object):
+    def __init__(self, tuples):
+        self.tuples = tuples
+        self.calls = 0
+
+    def call(self, lookup_names=True):
+        self.calls += 1
+        return self.tuples
+
+
+class FindSpheroTest(unittest.TestCase):
+    def tearDown(self):
+        reload(bluetooth)
+
+    def test_find_one(self):
+        override = OverrideDiscover([(33, "Sphero-YBO")])
+        bluetooth.discover_devices = override.call
+        answer = BluetoothWrapper.find_free_spheros()
+        self.assertEqual(answer["Sphero-YBO"], 33)
+
+    def test_multi_calls(self):
+        override = OverrideDiscover([])
+        bluetooth.discover_devices = override.call
+        BluetoothWrapper.find_free_spheros(tries=10)
+        self.assertEqual(override.calls, 10)
+
+    def test_no_match(self):
+        override = OverrideDiscover([(1234, "NOT_A_MATCH")])
+        bluetooth.discover_devices = override.call
+        answer = BluetoothWrapper.find_free_spheros()
+        self.assertEqual(len(answer), 0)
+
+
+class SocketStub(object):
+    def __init__(self, connection_t):
+        self.connection_t = connection_t
+        self.address = None
+        self.port = 0
+
+    def connect(self, address_t):
+        if address_t == "fail":
+            raise bluetooth.BluetoothError("TestFail")
+
+        address, port = address_t
+        self.address = address
+        self.port = port
+
+    def close(self):
+        pass
+
+
+class BlueToothWrapperConnectTest(unittest.TestCase):
+    def setUp(self):
+        bluetooth.BluetoothSocket = SocketStub
+        self.wrapper = BluetoothWrapper()
+
+    def tearDown(self):
+        reload(bluetooth)
+
+    def test_sucess(self):
+        result = self.wrapper.connect(address="some_address")
+        self.assertTrue(result)
+
+    def test_error(self):
+        with self.assertRaises(SpheroException):
+            self.wrapper.connect(address="fail")
