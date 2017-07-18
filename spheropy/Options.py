4bdbdd33bb83a4a8c30e980aa88cc211d339ab4f
Darin Costello <dran226@gmail.com>
2017-07-18 17:57:50 -0600

    Fixed indents in Perm Options

--- spheropy/Options.py
+++ spheropy/Options.py
@@ -1,3 +1,7 @@
+"""
+Tools for working with Sphero Options
+"""
+
 BIT0 = 0x00000001
 BIT1 = 0x00000002
 BIT2 = 0x00000004
@@ -18,108 +22,117 @@ class PermanentOptions(object):
     def __init__(self):
         self.bitflags = 0x00000000
 
-        @property
-        def sleep_on_charge_connected(self):
-            """
-            Prevents the sphero from immediatly sleeping when placed on the charged and connected over bluetooth
-            """
-            return bool(self.bitflags & BIT0)
+    def set_light_wakeup_sensitivity(self):
+        """
+        sets wakeup sensitivity to 'light'
+        """
+        self.bitflags &= (~ BIT7)
+        self.bitflags |= BIT6
 
-        @sleep_on_charge_connected.setter
-        def sleep_on_charge_connected(self, value):
-            if value:
-                self.bitflags |= BIT0
-            else:
-                self.bitflags &= (~ BIT0)
+    def set_heavy_wakeup_sensitivity(self):
+        """
+        sets wakeup sensitivity to 'heavy'
+        """
+        self.bitflags &= (~ BIT6)
+        self.bitflags |= BIT7
 
-        @property
-        def vector_drive(self):
-            """
-            Set to enable when Sphero  is stopped and ais given a new roll command, ti achieves teh heading before moving along it.
-            """
-            return bool(self.bitflags & BIT1)
+    @property
+    def sleep_on_charge_connected(self):
+        """
+        Prevents the sphero from immediatly sleeping when
+        placed on the charger and connected over bluetooth
+        """
+        return bool(self.bitflags & BIT0)
 
-        @vector_drive.setter
-        def vector_drive(self, value):
-            if value:
-                self.bitflags |= BIT1
-            else:
-                self.bitflags &= (~ BIT1)
+    @sleep_on_charge_connected.setter
+    def sleep_on_charge_connected(self, value):
+        if value:
+            self.bitflags |= BIT0
+        else:
+            self.bitflags &= (~ BIT0)
 
-        @property
-        def level_on_charge(self):
-            """
-            set to diable self_leveling when sphero is palced on the charger
-            """
-            return bool(self.bitflags & BIT2)
+    @property
+    def vector_drive(self):
+        """
+        Set to enable when Sphero  is stopped and ais given a new roll command
+        it achieves the heading before moving along it.
+        """
+        return bool(self.bitflags & BIT1)
 
-        @level_on_charge.setter
-        def level_on_charge(self, value):
-            if value:
-                self.bitflags |= BIT2
-            else:
-                self.bitflags &= (~ BIT2)
+    @vector_drive.setter
+    def vector_drive(self, value):
+        if value:
+            self.bitflags |= BIT1
+        else:
+            self.bitflags &= (~ BIT1)
 
-        @property
-        def tail_always_on(self):
-            """
-            set to force the tail led always on
-            """
-            return bool(self.bitflags & BIT3)
+    @property
+    def level_on_charge(self):
+        """
+        set to diable self_leveling when sphero is palced on the charger
+        """
+        return bool(self.bitflags & BIT2)
 
-        @tail_always_on.setter
-        def tail_always_on(self, value):
-            if value:
-                self.bitflags |= BIT3
-            else:
-                self.bitflags &= (~ BIT3)
+    @level_on_charge.setter
+    def level_on_charge(self, value):
+        if value:
+            self.bitflags |= BIT2
+        else:
+            self.bitflags &= (~ BIT2)
 
-        @property
-        def enable_motion_timeout(self):
-            """
-            set to enable motion timeout
-            """
-            return bool(self.bitflags & BIT4)
+    @property
+    def tail_always_on(self):
+        """
+        set to force the tail led always on
+        """
+        return bool(self.bitflags & BIT3)
 
-        @enable_motion_timeout.setter
-        def enable_motion_timeout(self, value):
-            if value:
-                self.bitflags |= BIT4
-            else:
-                self.bitflags &= (~ BIT4)
+    @tail_always_on.setter
+    def tail_always_on(self, value):
+        if value:
+            self.bitflags |= BIT3
+        else:
+            self.bitflags &= (~ BIT3)
 
-        @property
-        def demo_mode(self):
-            """
-            set to enable demo retail mode, ball runs a slow rainbow for 60 minutes when palced in charger
-            """
-            return bool(self.bitflags & BIT5)
+    @property
+    def enable_motion_timeout(self):
+        """
+        set to enable motion timeout
+        """
+        return bool(self.bitflags & BIT4)
 
-        @demo_mode.setter
-        def demo_mode(self, value):
-            if value:
-                self.bitflags |= BIT5
-            else:
-                self.bitflags &= (~ BIT5)
+    @enable_motion_timeout.setter
+    def enable_motion_timeout(self, value):
+        if value:
+            self.bitflags |= BIT4
+        else:
+            self.bitflags &= (~ BIT4)
 
-        def set_light_sensitivity(self):
-            self._bitflag &= (~ BIT7)
-            self._bitflag |= BIT6
+    @property
+    def demo_mode(self):
+        """
+        set to enable demo retail mode,
+        ball runs a slow rainbow for 60 minutes when palced in charger
+        """
+        return bool(self.bitflags & BIT5)
 
-        def set_heavy_awake_sensitivity(self):
-            self._bitflag &= (~ BIT6)
-            self._bigflag |= BIT7
+    @demo_mode.setter
+    def demo_mode(self, value):
+        if value:
+            self.bitflags |= BIT5
+        else:
+            self.bitflags &= (~ BIT5)
 
-        @property
-        def enable_gyro_max_async_msg(self):
-            """
-            enables the gyro max async message
-            """
-            return bool(self.bitflag & BIT8)
+    @property
+    def enable_gyro_max_async_msg(self):
+        """
+        enables the gyro max async message
+        """
+        return bool(self.bitflags & BIT8)
 
-        @enable_gyro_max_async_msg.setter
-        def enable_gyuro_max_async_msg(self, value):
-            if value:
-                self.bitfalgs |= BIT8
-            else:
-                self.bitflags &= (~ BIT8)
+    @enable_gyro_max_async_msg.setter
+    def enable_gyuro_max_async_msg(self, value):
+        if value:
+            self.bitflags |= BIT8
+        else:
+            self.bitflags &= (~ BIT8)
