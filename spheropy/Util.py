8fd37cf87f5d6fb44a5864a02362339debe89a80
Darin Costello <dran226@gmail.com>
2017-08-10 18:12:05 -0600

    updated util docs

--- spheropy/Util.py
+++ spheropy/Util.py
@@ -3,14 +3,28 @@ import sys
 
 
 def nothing(data):
-    print(data)
+    """
+    A function that does nothing. Used as the default callback for async events
+    """
+    pass
 
 
 def outside_range(number, min_range, max_range):
+    """
+    Returns True if `number` is between `min_range` and `max_range` exclusive.
+    """
     return number < min_range or number > max_range
 
 
 def int_to_bytes(number, length):
+    """
+    Returns a list of bytes that repersent `number`. The list is little endian.
+
+    `length` represents the number of bytes.
+
+    `number` is cast to an int before using. If number is larger then `length` 
+    the higher order bytes are ignored.
+    """
     number = int(number)
     result = []
     for i in range(0, length):
@@ -21,7 +35,9 @@ def int_to_bytes(number, length):
 
 def check_sum(data):
     """
-    calculates the checksum as The modulo 256 sum of all the bytes bit inverted (1's complement)
+    Calculates a checksum as the modulo 256 sum then bit inverted (1's complement).
+
+    `data` is an iterable list of bytes or ints < 256
     """
     return (sum(data) % 256) ^ 0xff
 
